505a3bbb04e6b4c8aad128432c32ae6f44c6324f
nuco-mrmt <daiki_marumoto@nuco.co.jp>
2025-03-17 14:04:03 +0900

    add get_location.py

---
+++ app/get_location.py
@@ -0,0 +1,37 @@
+import requests
+
+def get_coordinate(place_name, prefecture= ""):
+    """
+    国土地理院APIを使用して、住所から緯度経度を取得する関数。
+    """
+    url = "https://msearch.gsi.go.jp/address-search/AddressSearch"
+    params = {"q": place_name}
+    r = requests.get(url, params=params)
+    data = r.json()
+    print(f"r: {r}")
+    if "error" in data:
+        print(data["error"])
+        return None, None
+    if not data:
+        return None, None
+    else:
+        # レスポンスと施設名が一致する緯度経度を返す
+        for row in data:
+            if row["properties"]["title"].startswith(place_name):
+                coordinate = row["geometry"]["coordinates"]
+                title = row["properties"]["title"]
+                return coordinate, title
+        # レスポンス値と都道府県が一致する緯度経度を返す
+        for row in data:
+            if row["properties"]["title"].startswith(prefecture):
+                coordinates = row["geometry"]["coordinates"]
+                title = row["properties"]["title"]
+                return coordinates, title
+        # 見つからない場合
+        return None, None
+       
+coordinates1,title1 = get_coordinate("秋芳洞")
+print("有名施設：",coordinates1,title1)
+
+# 秋芳洞 34.22795,131.303069
+
--- dockerfile
+++ dockerfile
@@ -8,4 +8,4 @@ RUN pip install --no-cache-dir --upgrade -r /usr/src/app/requirements.txt
 
 COPY ./app /usr/src/app
 
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "80"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
