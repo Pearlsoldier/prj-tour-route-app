654cf95244052d11c8d2ee0b0061aa4af3b4915d
nuco-mrmt <daiki_marumoto@nuco.co.jp>
2025-03-25 19:12:38 +0900

    modified locations

--- app/location/locations.py
+++ app/location/locations.py
@@ -2,36 +2,54 @@ import requests
 
 
 class GeocodingService:
-    def __init__(self):
+    def __init__(self, url, place_name):
+        self.url = "https://msearch.gsi.go.jp/address-search/AddressSearch"
+        self.params = {"q": place_name}
+        self.r = requests.get(url, params=params)
+        self.data = r.json()
+
+    def get_coordinates(place_name: str) -> list:
+        """
+        国土地理院APIを使用して、住所から緯度経度を取得する関数。
+        """
+        if "error" in data:
+            return None, None
+        if not data:
+            return None, None
+        else:
+            # レスポンスと施設名が一致する緯度経度を返す
+            for row in data:
+                if row["properties"]["title"].startswith(place_name):
+                    coordinate = row["geometry"]["coordinates"]
+                    title = row["properties"]["title"]
+                    return coordinate, title
+            return None, None
+
+    def get_location(lon: float, lat: float) -> str:
+        """
+        経度緯度から地点名に変換する関数
+            longitude (float): 経度
+            latitude (float): 緯度
+        """
         pass
 
 
-def convert_to_coordinates(place_name: str) -> list:
-    """
-    国土地理院APIを使用して、住所から緯度経度を取得する関数。
-    """
-    url = "https://msearch.gsi.go.jp/address-search/AddressSearch"
-    params = {"q": place_name}
-    r = requests.get(url, params=params)
-    data = r.json()
-    if "error" in data:
-        return None, None
-    if not data:
-        return None, None
-    else:
-        # レスポンスと施設名が一致する緯度経度を返す
-        for row in data:
-            if row["properties"]["title"].startswith(place_name):
-                coordinate = row["geometry"]["coordinates"]
-                title = row["properties"]["title"]
-                return coordinate, title
-        return None, None
+
+class CurrentLocation:
+    def __init__(self, current_location):
+        self.current_location = current_location
+
+class NextLocation:
+    def __init__(self, next_location):
+        self.next_location = next_location
+
+
 
 
 # 秋芳洞 34.22795,131.303069
 
 
-def convert_to_location(lon: float, lat: float) -> str:
+def get_location(lon: float, lat: float) -> str:
     """
     経度緯度から地点名に変換する関数
         longitude (float): 経度
@@ -41,9 +59,13 @@ def convert_to_location(lon: float, lat: float) -> str:
 
 
 def main():
-    place = "厳島神社"
-    point = convert_to_coordinates(place)
-    print(point)
+    place_name = "厳島神社"
+    location_instance = CurrentLocation(place_name)
+    print(location_instance.current_location)
+
+    place_name = "秋芳洞"
+    location_instance = NextLocation(place_name)
+    print(location_instance.next_location)
 
 
 if __name__ == "__main__":
