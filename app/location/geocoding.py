239da1a0479025d002071f330685319237cc61b6
abe <abeh9197@gmail.com>
2025-03-31 15:34:29 +0900

    Geocodingクラスの修正

--- app/location/geocoding.py
+++ app/location/geocoding.py
@@ -2,35 +2,24 @@ import requests
 
 
 class Geocoding:
-    def __init__(self, place_name: str):
-        self.place = place_name
-        url = f"https://msearch.gsi.go.jp/address-search/AddressSearch?q={self.place}"
-        params = {"q": place_name}
-        r = requests.get(url, params=params)
-        self.data = r.json()
+    def __init__(self):
+        self.api_url = "https://msearch.gsi.go.jp/address-search/AddressSearch?q={place}"
 
-    def get_coordinate(self) -> list:
+    def get_coordinate(self, place: str) -> list[float]:
         """
         ジオコーディング
         国土地理院APIを使用して、住所から緯度経度を取得する関数。
         """
-        if "error" in self.data:
+        response = requests.get(self.api_url.format(place=place), params={"q": place}).json()
+
+        if "error" in response:
             return "error data"
-        if not self.data:
+        if not response:
             return "not data"
         else:
             # レスポンスと施設名が一致する緯度経度を返す
-            for row in self.data:
-                if row["properties"]["title"].startswith(self.place):
+            for row in response:
+                if row["properties"]["title"].startswith(place):
                     coordinates = row["geometry"]["coordinates"]
                     return coordinates
             return None
-
-def main():
-    geo = Geocoding("東京駅")
-    result = geo.get_coordinate()
-    print(result)
-
-
-if __name__ == "__main__":
-    main()
