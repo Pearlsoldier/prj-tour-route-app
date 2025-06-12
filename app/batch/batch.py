306e2841dcf7a7994b868611bc5af339fa331ce9
Pearlsoldier <marumotodaiki@gmail.com>
2025-06-13 08:22:17 +0900

    get locations_table

--- app/batch/batch.py
+++ app/batch/batch.py
@@ -3,26 +3,27 @@ import os
 import pprint
 import uuid
 
+import sys
+import os
 
-from geocoding.geocoding import Geocoding, ReverseGeocoding
-from location.locations import Location, AccessibleLocation
-from interface.input_parser import (
-    InterfaceAdministrator,
-    InterfaceBatch,
-    CidInterfaceBatch,
-)
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from transport.transport import Walk
-from transittime.timerequired import TimeRequired
+
 from DB.database import DatabaseService
 from sql.postgresql import QueryBuilder
-from batch.preprocessing import DatabasePreprocessing
+from preprocessing import DatabasePreprocessing
 
 
 def main():
 
     sql_handler = QueryBuilder()
-    batch_insert = sql_handler.insert_cid_datasets()
+
+    uuid.uuid4
+    # batch_insert = sql_handler.insert_cid_datasets()
+    locations_tabale_query = sql_handler.get_locations_table()
+    db_handler = DatabaseService()
+    locations_table = db_handler.execute_query_fetch(locations_tabale_query)
+    print(f"locations_table: {locations_table}")
 
 
 if __name__ == "__main__":
