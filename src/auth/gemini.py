e155287d7675bc5773b7e50dff38302957d8b4f6
Pearlsoldier <marumotodaiki@gmail.com>
2025-06-30 23:51:20 +0900

    Geminiの実装を始める。

---
+++ src/auth/gemini.py
@@ -0,0 +1,11 @@
+import os
+
+from dotenv import load_dotenv
+
+class GeminiSecrets:
+    def __init__(self) -> None:
+        self.__api_key = load_local_env()
+    
+    @property
+    def api_key(self) -> str:
+        return self.__api_key
