cd271463fb9da319a1e897a84a93d26dd8d98812
Pearlsoldier <marumotodaiki@gmail.com>
2025-07-11 00:59:56 +0900

    LLMクラスの設計

--- app/llm/gemini.py
+++ app/llm/gemini.py
@@ -17,22 +17,45 @@ API_KEY = os.getenv("API_KEY")
 
 
 class GeminiResponse(BaseModel):
+    """
+    Geminiのレスポンスを規定するクラス
+    """
     response: str
     is_continue_conversation: bool
 
 
+class ConversationMessa(BaseModel):
+    """
+    会話の１メッセージを規定する
+    roleは、ユーザーかGeminiか
+    """
+    role: str
+    content: str
+
+
+class SessionManager(BaseModel):
+    """
+    Geminiとユーザーの会話を管理するクラス
+    """
+
+
+"""
 # クライアントの初期化
-client = genai.Client(api_key=API_KEY)
+    @property
+    def client(self, API_KEY):
+        self.client = genai.Client(api_key=API_KEY)
 
 # コンテンツ生成の実行
-response = client.models.generate_content(
-    model="gemini-2.0-flash-exp",
-    contents=user_prompt,
-    config=genai.types.GenerateContentConfig(
-        system_instruction=system_prompt,
-        response_mime_type="application/json",
-        response_schema=GeminiResponse.model_json_schema(),
-    ),
-)
-
-# 結果の表示
+    @property
+    def response(self, user_prompt, system_prompt):
+        response = self.client.models.generate_content(
+            model="gemini-2.0-flash-exp",
+            contents=user_prompt,
+            config=genai.types.GenerateContentConfig(
+                system_instruction=system_prompt,
+                response_mime_type="application/json",
+                response_schema=GeminiResponse.model_json_schema(),
+                ),
+            )
+        return response
+"""
