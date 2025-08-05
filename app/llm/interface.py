fd893c957c355ddefa18801867cc8fdc81be7408
Pearlsoldier <marumotodaiki@gmail.com>
2025-08-06 01:08:41 +0900

    実装途中：ChatInterfaceの実装

--- app/llm/interface.py
+++ app/llm/interface.py
@@ -7,9 +7,20 @@ from prompts import system_prompt, user_prompt
 
 from client import GeminiClient
 
-class llm_interface:
-    def __init__(self, user_input):
-        pass
 
-    def llm_response(self):
-        pass
+class ChatInterface:
+    def __init__(
+        self,
+        response_schema,
+        contents_formatter,
+        client,
+        system_instruction,
+        user_input,
+        chat_logs,
+    ):
+        self._response_schema = response_schema
+        self._contents_formatter = contents_formatter
+        self._client = client
+        self._system_instruction = system_instruction
+        self._user_input = user_input
+        self._chat_logs = chat_logs
